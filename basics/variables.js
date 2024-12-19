86321961eae9a78d2cad5aecb6a79d9bf7dfefd6
mafzalsahi <135060790+mafzalsahi@users.noreply.github.com>
2024-12-19 08:32:03 +0000

    container config done

---
+++ basics/variables.js
@@ -0,0 +1,10 @@
+const studentId=124356
+let studentEmail="student@yahoo.com"
+var studentPassword="1111"
+studentCity="Manama"
+//studentId=35666 //doesn't allow assigning value again
+studentEmail="updatedstudent@yahoo.com"
+studentPassword="2222"
+studentCity="Riffa"
+let studentState;
+console.table([studentId,studentEmail,studentPassword,studentCity,studentState])
